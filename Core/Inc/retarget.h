f3ed09aa116095029a520cef5789921d5e96098e
xiao_wine <68009112+xiaowine@users.noreply.github.com>
2024-08-16 17:32:32 +0800

    opt

--- Core/Inc/retarget.h
+++ Core/Inc/retarget.h
@@ -8,11 +8,7 @@
 #include <sys/stat.h>
 
 void RetargetInit(UART_HandleTypeDef *huart);
-int _isatty(int fd);
 int _write(int fd, char* ptr, int len);
-int _close(int fd);
-int _lseek(int fd, int ptr, int dir);
 int _read(int fd, char* ptr, int len);
-int _fstat(int fd, struct stat* st);
 
 #endif //#ifndef _RETARGET_H__
